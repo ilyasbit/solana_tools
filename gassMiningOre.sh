9d6afc2649f763bced1baabdf63227d3ed0a5171
Ilyas Fauzal Akbar <66202601+ilyasbit@users.noreply.github.com>
2024-04-11 09:11:13 +0700

    Update gassMiningOre.sh

--- gassMiningOre.sh
+++ gassMiningOre.sh
@@ -6,7 +6,8 @@ declare -i threads=$3
 wallets=($(ls $dir/*.json))
 
 totalWallets=${#wallets[@]}
-
+touch ~/ore.log
+echo "" >~/ore.log
 runExecUrl='https://github.com/ilyasbit/solana_tools/raw/main/run.sh'
 if ! [ -x "$(command -v runOre)" ]; then
   curl -L $runExecUrl -o /usr/local/bin/runOre
@@ -19,7 +20,7 @@ function mining {
   wallet=$3
   for i in $(seq 1 $threads); do
     fileName=$(basename $wallet)
-    echo "Mining wallet $wallet thread $i | to check the progress run: screen -r $fileName-$i" >>~/mining.log
+    echo "Mining wallet $wallet thread $i | to check the progress run: screen -r $fileName-$i" | tee -a ~/ore.log
     echo "screen -dmS $fileName-$i runOre $wallet $rpcFile"
     sleep 60
   done
@@ -27,6 +28,6 @@ function mining {
 
 export -f mining
 
-parallel --delay 30 -j $totalWallets mining ::: $rpcFile ::: $threads ::: "${wallets[@]}"
+parallel --delay 20 -j $totalWallets mining ::: $rpcFile ::: $threads ::: "${wallets[@]}"
 
 echo "Running $totalWallets wallets with $threads threads each Done."
