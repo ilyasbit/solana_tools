6c7d833e72b32822e8ad3ae7cbe432edef5467ff
Ilyas Fauzal Akbar <66202601+ilyasbit@users.noreply.github.com>
2024-04-11 07:45:19 +0700

    Update installOreMiner.sh

--- installOreMiner.sh
+++ installOreMiner.sh
@@ -1,7 +1,7 @@
 #!/bin/env bash
 
 apt update && apt upgrade -y
-apt install parallel jq -y
+apt install parallel jq build-essential -y
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 . "$HOME/.cargo/env"
 cargo install ore-cli
